ba0cf02e85edfa057b29d2c996ffb000edc5377a
Daniel Baker <71296921+danb127@users.noreply.github.com>
2023-08-01 10:21:52 -0400

    SMV Alert Testing

---
+++ alert_system.py
@@ -0,0 +1,46 @@
+
+from config import OUTLET_PSI_RANGES
+from test import TEST_PSI_RANGES
+import pandas as pd
+
+def check_psi(focus, range):
+    print(focus, range)
+    
+    l, h = range[0], range[1]
+    
+    if focus >= l and focus <= h:
+        print(f"good")
+    elif focus < l:
+        print(f"Too low")
+    elif focus > h:
+        print(f"Too high")
+    else:
+        print(f"Error")
+...
+def analyze_file(filepath):
+    # Load the excel data
+    # For now, just read the file and check its psi values
+    # You might want to expand this further based on the actual analysis
+    data = pd.read_excel(filepath)  # Use Pandas to read the Excel file
+    feedback = ""
+
+    for key in TEST_PSI_RANGES:
+        if key in OUTLET_PSI_RANGES:
+            focus = TEST_PSI_RANGES[key]
+            result = check_psi(focus, OUTLET_PSI_RANGES[key])
+            feedback += f"Key: {key}, Result: {result}\n"
+
+    return feedback
+...
+
+if __name__ == "__main__":
+    file = input("Enter file name: ")
+    
+    print("This is the main function.")
+    for key in TEST_PSI_RANGES:
+        if key in OUTLET_PSI_RANGES:
+            focus = TEST_PSI_RANGES[key]
+            check_psi(focus, OUTLET_PSI_RANGES[key])
+            
+        
+        # check_psi(list(OUTLET_PSI_RANGES.keys())[i], list(OUTLET_PSI_RANGES.values())[i])
--- data_import.py
+++ data_import.py
@@ -2,26 +2,28 @@ import pandas as pd
 import os
 import glob
 
+# import the configuration
+from config import STANDARD_MODEL_PATH
+
 # relative directory where your Excel files are stored
-data_dir = './Pull Complaint Valve Data Pat M'
+data_dir = os.path.dirname(STANDARD_MODEL_PATH)
 
 def import_data(data_dir):
     # get a list of all Excel files in the directory
-    files = glob.glob(os.path.join(data_dir, '*.xlsx'))
+    blockage_files = glob.glob(os.path.join(data_dir, '*.xlsx'))
     
     # create an empty list to hold the dataframes
-    dataframes = []
+    blockage_dataframes = []
     
-    for file in files:
+    for file in blockage_files:
         # read the Excel file and add it to the list
         df = pd.read_excel(file)
-        dataframes.append(df)
+        blockage_dataframes.append(df)
+    
+    # read standard file
+    standard_df = pd.read_excel(STANDARD_MODEL_PATH)    
         
-        # print the first few rows of the dataframe
-        print(df.head())
-        
-        
-    return dataframes
+    return blockage_dataframes, standard_df
 
 # use the function
-dataframes = import_data(data_dir)
+blockage_dataframes, standard_df = import_data(data_dir)
